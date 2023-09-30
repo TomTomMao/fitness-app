73c27e19860bb1250f2006bc33d021713b81af0a
Wentao Mao <wentaom@foxmail.com>
2023-09-30 15:19:02 +0100

    built the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
+import NavBar from "@/scenes/navbar"
+import Home from "@/scenes/home"
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/share/types";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage, setIsTopPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopPage(true);
+        setSelectedPage(SelectedPage.Home)
+      } else {
+        setIsTopPage(false);
+      }
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, []);
+
   return (
-    <div className='app'>app</div>
+    <div className='app bg-gray-20'>
+      <NavBar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage}/>
+    </div>
   )
 }
 
-export default App
+export default App;
