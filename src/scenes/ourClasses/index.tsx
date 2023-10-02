3d288936437aa810037d2533092fb43b32ea22a9
Wentao Mao <wentaom@foxmail.com>
2023-10-02 18:43:41 +0100

    removed type errors

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -1,5 +1,5 @@
 import { ClassType, SelectedPage } from '@/shared/types'
-import React from 'react'
+
 import image1 from "@/assets/image1.png"
 import image2 from "@/assets/image2.png"
 import image3 from "@/assets/image3.png"
